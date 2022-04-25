8b8dce488184552a1d0dfbc63cc5fd61af73ca72
su <1234567@126.com>
2022-04-25 09:08:29 +0800

    昨天的作业

---
+++ day5/src/http/env.js
@@ -0,0 +1,5 @@
+export default {
+    prod: {
+        baseURL: "http://www.ysqorz.top:8888/api/private/v1"
+    }
+}
---
+++ day5/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='home'>
+      
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style lang='scss'>
+</style>

0b8abfe2be31506c5b9b89acd7934e54c451a8f4
su <1234567@126.com>
2022-04-25 08:48:45 +0800

    js

--- day05/src/http/api.js
+++
@@ -1,8 +0,0 @@
-import request from "./request"
-export function LoginApi(data) {
-    return request({
-        url: '/login',
-        method: "POST",
-        data
-    })
-}
--- day05/src/http/env.js
+++
@@ -1,5 +0,0 @@
-export default {
-    prod: {
-        baseURL: "http://www.ysqorz.top:8888/api/private/v1"
-    }
-}
